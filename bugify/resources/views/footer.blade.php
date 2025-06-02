dd76a9c8f62893d691abf764d990f6f49caf052e
HamzaDavinci <99075941@mydavinci.nl>
2025-06-02 23:15:48 +0200

    tiny footer update

--- bugify/resources/views/footer.blade.php
+++ bugify/resources/views/footer.blade.php
@@ -1,3 +1,6 @@
 <footer class="bg-background-surface p-4 text-center text-sm text-gray-500">
-    © <?php echo(date("Y"))?> Bugify Inc. Proudly broken. - Made By Hamzq
+    © <?php echo(date("Y"))?> Bugify Inc. Proudly broken. - Made By 
+    <a href="https://hamzq.dev" target="_blank" rel="noopener noreferrer" class="text-spotify-green hover:underline">
+        Hamzq
+    </a>
 </footer>
